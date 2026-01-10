75a7ea52466aaeb2f17a33ba5894fcacf0fd2bfd
Jiaye Zou <tony200303.zt@gmail.com>
2026-01-10 12:12:17 -0800

    init

--- build_files/build.sh
+++ build_files/build.sh
@@ -4,21 +4,109 @@ set -ouex pipefail
 
 ### Install packages
 
-# Packages can be installed from any enabled yum repo on the image.
-# RPMfusion repos are available by default in ublue main images
-# List of rpmfusion packages can be found here:
-# https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/43/x86_64/repoview/index.html&protocol=https&redirect=1
+systemctl enable systemd-timesyncd
+systemctl enable systemd-resolved.service
 
-# this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf -y install 'dnf5-command(config-manager)'
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+dnf -y remove \
+  console-login-helper-messages \
+  chrony \
+  sssd* \
+  qemu-user-static* \
+  toolbox
 
-#### Example for enabling a System Unit File
+dnf -y install \
+  -x PackageKit* \
+  NetworkManager \
+  NetworkManager-adsl \
+  NetworkManager-bluetooth \
+  NetworkManager-config-connectivity-fedora \
+  NetworkManager-libnm \
+  NetworkManager-openconnect \
+  NetworkManager-openvpn \
+  NetworkManager-strongswan \
+  NetworkManager-ssh \
+  NetworkManager-ssh-selinux \
+  NetworkManager-vpnc \
+  NetworkManager-wifi \
+  NetworkManager-wwan \
+  alsa-firmware \
+  alsa-sof-firmware \
+  alsa-tools-firmware \
+  atheros-firmware \
+  audispd-plugins \
+  audit \
+  brcmfmac-firmware \
+  cifs-utils \
+  cups \
+  cups-pk-helper \
+  dymo-cups-drivers \
+  firewalld \
+  fprintd \
+  fprintd-pam \
+  fuse \
+  fuse-common \
+  fwupd \
+  gum \
+  gvfs-archive \
+  gvfs-mtp \
+  gvfs-nfs \
+  gvfs-smb \
+  hplip \
+  hyperv-daemons \
+  ibus \
+  ifuse \
+  intel-audio-firmware \
+  iwlegacy-firmware \
+  iwlwifi-dvm-firmware \
+  iwlwifi-mvm-firmware \
+  jmtpfs \
+  kernel-modules-extra \
+  libcamera{,-{v4l2,gstreamer,tools}} \
+  libimobiledevice \
+  libimobiledevice-utils \
+  libratbag-ratbagd \
+  man-db \
+  man-pages \
+  mobile-broadband-provider-info \
+  mt7xxx-firmware \
+  nxpwireless-firmware \
+  open-vm-tools \
+  open-vm-tools-desktop \
+  openconnect \
+  pam_yubico \
+  pcsc-lite \
+  plymouth \
+  plymouth-system-theme \
+  printer-driver-brlaser \
+  ptouch-driver \
+  qemu-guest-agent \
+  realtek-firmware \
+  rsync \
+  spice-vdagent \
+  steam-devices \
+  switcheroo-control \
+  system-config-printer-libs \
+  system-config-printer-udev \
+  systemd-container \
+  systemd-oomd-defaults \
+  tiwilink-firmware \
+  tuned \
+  tuned-ppd \
+  unzip \
+  usb_modeswitch \
+  uxplay \
+  vpnc \
+  whois \
+  wireguard-tools \
+  zram-generator-defaults
 
+sed -i 's|^ExecStart=.*|ExecStart=/usr/bin/bootc update --quiet|' /usr/lib/systemd/system/bootc-fetch-apply-updates.service
+sed -i 's|^OnUnitInactiveSec=.*|OnUnitInactiveSec=7d\nPersistent=true|' /usr/lib/systemd/system/bootc-fetch-apply-updates.timer
+sed -i 's|#AutomaticUpdatePolicy.*|AutomaticUpdatePolicy=stage|' /etc/rpm-ostreed.conf
+
+systemctl enable bootc-fetch-apply-updates
+systemctl enable auditd
+systemctl enable firewalld
 systemctl enable podman.socket
