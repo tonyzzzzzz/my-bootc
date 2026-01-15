1c252d24ae24c968336b3d4aa5eeec9805c4a5a8
Jiaye Zou <tony200303.zt@gmail.com>
2026-01-15 13:00:41 -0800

    update: nv

--- Containerfile
+++ Containerfile
@@ -56,9 +56,6 @@ RUN --mount=type=cache,dst=/var/cache \
     /usr/bin/systemctl preset brew-update.timer && \
     /usr/bin/systemctl preset brew-upgrade.timer
 RUN --mount=type=bind,from=ctx,source=/,target=/ctx \
-    --mount=type=cache,dst=/var/cache \
-    --mount=type=cache,dst=/var/log \
-    --mount=type=tmpfs,dst=/tmp \
     /ctx/build/nvidia.sh
 
 
--- build_files/nvidia.sh
+++ build_files/nvidia.sh
@@ -1,20 +1,26 @@
-#!/bin/bash
-dnf -y install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf -y update
-dnf -y install kernel-devel akmod-nvidia xorg-x11-drv-nvidia-cuda
+#!/usr/bin/env bash
 
-tee /usr/lib/modprobe.d/00-nouveau-blacklist.conf <<'EOF'
-blacklist nouveau
-options nouveau modeset=0
-EOF
+if [[ ! "${BUILD_FLAVOR}" =~ "nvidia" ]]; then
+  exit 0
+fi
 
-tee /usr/lib/bootc/kargs.d/00-nvidia.toml <<'EOF'
-kargs = ["i915.enable_dpcd_backlight=1", "nvidia.NVreg_EnableBacklightHandler=0", "nvidia.NVreg_RegistryDwords=EnableBrightnessControl=0", "rd.driver.blacklist=nouveau", "modprobe.blacklist=nouveau", "nvidia-drm.modeset=1"]
-EOF
+set -xeuo pipefail
 
-dnf -y copr enable lukenukem/asus-linux
-dnf -y copr disable lukenukem/asus-linux
-dnf -y --enablerepo copr:copr.fedorainfracloud.org:lukenukem:asus-linux install asusctl supergfxctl
+KERNEL_VERSION="$(find "/usr/lib/modules" -maxdepth 1 -type d ! -path "/usr/lib/modules" -exec basename '{}' ';' | sort | tail -n 1)"
+
+dnf config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-nvidia.repo
+dnf config-manager setopt fedora-nvidia.enabled=0
+sed -i '/^enabled=/a\priority=90' /etc/yum.repos.d/fedora-nvidia.repo
+
+dnf -y install --enablerepo=fedora-nvidia akmod-nvidia
+mkdir -p /var/tmp # for akmods
+chmod 1777 /var/tmp
+dnf -y install gcc-c++
+akmods --force --kernels "${KERNEL_VERSION}" --kmod "nvidia"
+cat /var/cache/akmods/nvidia/*.failed.log || true
+
+dnf -y install --enablerepo=fedora-nvidia \
+  nvidia-driver-cuda libnvidia-fbc libva-nvidia-driver nvidia-driver nvidia-modprobe nvidia-persistenced nvidia-settings
 
 dnf config-manager addrepo --from-repofile=https://nvidia.github.io/libnvidia-container/stable/rpm/nvidia-container-toolkit.repo
 dnf config-manager setopt nvidia-container-toolkit.enabled=0
@@ -26,6 +32,22 @@ dnf -y install --enablerepo=nvidia-container-toolkit \
 curl --retry 3 -L https://raw.githubusercontent.com/NVIDIA/dgx-selinux/master/bin/RHEL9/nvidia-container.pp -o nvidia-container.pp
 semodule -i nvidia-container.pp
 rm -f nvidia-container.pp
+
+tee /usr/lib/modprobe.d/00-nouveau-blacklist.conf <<'EOF'
+blacklist nouveau
+options nouveau modeset=0
+EOF
+
+tee /usr/lib/bootc/kargs.d/00-nvidia.toml <<'EOF'
+kargs = ["rd.driver.blacklist=nouveau", "modprobe.blacklist=nouveau", "nvidia-drm.modeset=1"]
+EOF
+
+# Universal Blue specific Initramfs fixes
+mv /etc/modprobe.d/nvidia-modeset.conf /usr/lib/modprobe.d/nvidia-modeset.conf
+# we must force driver load to fix black screen on boot for nvidia desktops
+sed -i 's/omit_drivers/force_drivers/g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
+# as we need forced load, also must pre-load intel/amd iGPU else chromium web browsers fail to use hardware acceleration
+sed -i 's/ nvidia / i915 amdgpu nvidia /g' /usr/lib/dracut/dracut.conf.d/99-nvidia.conf
 
 tee /usr/lib/systemd/system/nvctk-cdi.service <<'EOF'
 [Unit]
@@ -40,6 +62,16 @@ ExecStart=/usr/bin/nvidia-ctk cdi generate --output=/etc/cdi/nvidia.yaml
 [Install]
 WantedBy=multi-user.target
 EOF
+
+systemctl enable nvctk-cdi.service
+systemctl disable akmods-keygen@akmods-keygen.service
+systemctl mask akmods-keygen@akmods-keygen.service
+systemctl disable akmods-keygen.target
+systemctl mask akmods-keygen.target
+
+dnf -y copr enable lukenukem/asus-linux
+dnf -y copr disable lukenukem/asus-linux
+dnf -y --enablerepo copr:copr.fedorainfracloud.org:lukenukem:asus-linux install asusctl supergfxctl
 
 systemctl enable supergfxd.service
 systemctl enable nvctk-cdi.service
